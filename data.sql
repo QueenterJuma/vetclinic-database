e86f21051a1bf6351b37b7efbc541ef9bda265ff
QueenterJuma <jumahqueenter@gmail.com>
2023-05-02 12:00:55 +0300

    Add data.sql

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (id, name, date_of_birth,escape_attempts, neutered, weight_kg) VALUES (1, 'Agumon', '02-03-20', 0, true, 10.23);
+INSERT INTO animals (id, name, date_of_birth,escape_attempts, neutered, weight_kg) VALUES (2, 'Gabumon', '01-01-18', 2, true, 8);
+INSERT INTO animals (id, name, date_of_birth,escape_attempts, neutered, weight_kg) VALUES (3, 'Pikachu', '01-17-21', 1, false, 15.04);
+INSERT INTO animals (id, name, date_of_birth,escape_attempts, neutered, weight_kg) VALUES (4, 'Devimon', '05-12-17', 5, true, 11);
