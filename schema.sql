56a92a0939658c24fe530c1dd849ac88596facae
QueenterJuma <jumahqueenter@gmail.com>
2023-05-04 06:09:39 +0300

    Add owner and species table in schema.sql

--- schema.sql
+++ schema.sql
@@ -9,3 +9,15 @@ CREATE TABLE animals (
     weight_kg decimal not null
 );
 ALTER TABLE animals ADD COLUMN species varchar(100);
+/* Create owners table */
+CREATE TABLE owners (id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL, full_name varchar(100), age int);
+
+/* Create species table */
+CREATE TABLE species (id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL, name varchar(100));
+
+/* Modify animals table (ID already set to auto_increment while creating the table on line no.5) */
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD COLUMN species_id int;
+ALTER TABLE animals ADD CONSTRAINT fk_name FOREIGN KEY(species_id) REFERENCES species(id);
+ALTER TABLE animals ADD owner_id int;
+ALTER TABLE animals ADD CONSTRAINT fk_owners FOREIGN KEY(owner_id) REFERENCES owners(id);
