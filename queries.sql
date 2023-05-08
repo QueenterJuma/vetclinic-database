d816594b943e0822f8083107a96880d913bdeb01
QueenterJuma <jumahqueenter@gmail.com>
2023-05-09 00:47:57 +0300

    Add update in queries files

--- queries.sql
+++ queries.sql
@@ -163,3 +163,10 @@ JOIN animals ON animals.id = visits.animal_id) as all_visits
 JOIN species ON all_visits.species_id = species.id
 GROUP BY name 
 ORDER BY total DESC LIMIT 1;
+
+
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4; 
+
+EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
+
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
